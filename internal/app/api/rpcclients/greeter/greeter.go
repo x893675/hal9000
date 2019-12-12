65130aeddbcf89383c7cba5768948a7226b5976b
x893675 <x893675@163.com>
2019-12-12 20:46:08 +0800

    working in test8

--- internal/app/api/rpcclients/greeter/greeter.go
+++ internal/app/api/rpcclients/greeter/greeter.go
@@ -2,18 +2,33 @@ package greeter
 
 import (
 	"github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/client/grpc"
 	"github.com/micro/go-plugins/client/selector/static"
 	"github.com/micro/go-plugins/registry/kubernetes"
-	"github.com/micro/go-plugins/transport/grpc"
 	"hal9000/proto/greeter"
 )
 
-func NewGreeterClient() greeter.GreeterService{
-	k := kubernetes.NewRegistry()
-	st := static.NewSelector()
-	return greeter.NewGreeterService("greeter", client.NewClient(
-		client.Registry(k),
-		client.Selector(st),
-		client.Transport(grpc.NewTransport(),
-	)))
+//import (
+//	"github.com/micro/go-micro/client"
+//	"github.com/micro/go-plugins/client/selector/static"
+//	"github.com/micro/go-plugins/registry/kubernetes"
+//	"github.com/micro/go-plugins/transport/grpc"
+//	"hal9000/proto/greeter"
+//)
+//
+//func NewGreeterClient() greeter.GreeterService{
+//	k := kubernetes.NewRegistry()
+//	st := static.NewSelector()
+//	return greeter.NewGreeterService("greeter", client.NewClient(
+//		client.Registry(k),
+//		client.Selector(st),
+//		client.Transport(grpc.NewTransport(),
+//	)))
+//}
+
+
+func NewGreeterClient()greeter.GreeterService{
+		k := kubernetes.NewRegistry()
+		st := static.NewSelector()
+		return greeter.NewGreeterService("greeter", grpc.NewClient(client.Registry(k), client.Selector(st)))
 }
