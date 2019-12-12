6f103ae91f796879bc1d9b5f3d3c3bbe77ecfe69
x893675 <x893675@163.com>
2019-12-12 17:08:25 +0800

    working in test4

--- cmd/greeter/greeter.go
+++ cmd/greeter/greeter.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/micro/go-micro"
+	"github.com/micro/go-plugins/transport/grpc"
 	srv "hal9000/internal/app/greeter"
 	k8s "hal9000/pkg/micro/micro"
 	"hal9000/proto/greeter"
@@ -16,6 +17,7 @@ func main(){
 		micro.Name("greeter"),
 		micro.RegisterTTL(time.Second*30),
 		micro.RegisterInterval(time.Second*10),
+		micro.Transport(grpc.NewTransport()),
 	)
 	service.Init()
 
