3b6af006b82cd6216c69969b6cb82c6be6bf138c
x893675 <x893675@163.com>
2019-12-12 16:56:45 +0800

    working in test3

--- internal/app/api/rpcclients/greeter/greeter.go
+++ internal/app/api/rpcclients/greeter/greeter.go
@@ -4,11 +4,12 @@ import (
 	"github.com/micro/go-micro/client"
 	"github.com/micro/go-plugins/client/selector/static"
 	"github.com/micro/go-plugins/registry/kubernetes"
+	"github.com/micro/go-plugins/transport/grpc"
 	"hal9000/proto/greeter"
 )
 
 func NewGreeterClient() greeter.GreeterService{
 	k := kubernetes.NewRegistry()
 	st := static.NewSelector()
-	return greeter.NewGreeterService("greeter", client.NewClient(client.Registry(k),client.Selector(st)))
+	return greeter.NewGreeterService("greeter", client.NewClient(client.Registry(k),client.Selector(st),client.Transport(grpc.NewTransport())))
 }
